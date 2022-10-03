ae42067465275a24c8c81f574fa5c35cc644dbc4
turalismylv <turalhi@code.edu.az>
2022-10-04 02:54:26 +0400

    HomeWork0410

---
+++ HomeWork0410/HomeWork0410/Controllers/UserController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HomeWork0410.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Name = "TURAL";
+            ViewBag.Surname = "Ismayilov";
+            ViewBag.Age = 21;
+            ViewBag.Country = "GEDEBEY";
+            ViewBag.PhoneNumber = "+994998596919";
+            return View();
+        }
+    }
+}
